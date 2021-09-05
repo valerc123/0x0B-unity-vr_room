1c85060b8909bc7e9aac7e6b9bcf692aa910057e
valerc123 <valentina.rua123@gmail.com>
2021-09-05 16:15:06 -0500

    active particles

---
+++ Assets/Scripts/particles.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class particles : MonoBehaviour
+{
+    public GameObject particlesSystem;
+    private bool switchParticle = true;
+
+    public void ActivateParticle()
+    {
+        particlesSystem.SetActive(switchParticle);
+        switchParticle = !switchParticle;
+        Debug.Log("entro");
+    }
+}
