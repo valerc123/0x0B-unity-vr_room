9027ae1cd58e11926ed71ced32511d41a68bc36d
valerc123 <valentina.rua123@gmail.com>
2021-09-05 15:28:47 -0500

    open door

---
+++ Assets/Scripts/openDoor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class openDoor : MonoBehaviour
+{
+    private Animator animator;
+    public bool switchAnimation = true;
+   
+    void Start(){
+        animator = GetComponent<Animator>();
+        animator.SetBool("character_nearby", false);
+    }
+    public void activeAnimation(){
+        animator.SetBool("character_nearby", switchAnimation);
+        switchAnimation = !switchAnimation;
+    }
+}
