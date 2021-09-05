22172f5f3a2515052b30bdc89e8a11991349e4fa
valerc123 <valentina.rua123@gmail.com>
2021-09-05 13:11:20 -0500

    grap object

---
+++ Assets/Scripts/grapInteractable.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class grapInteractable : MonoBehaviour
+{
+    public Material hover = null;
+    public Material Onhover = null;
+    private MeshRenderer meshRenderer = null;
+
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer.material = Onhover;
+    }
+
+    public void setColorOnhover(Collider other)
+    {
+        meshRenderer.material = Onhover;
+    }
+    public void setColorHover(Collider other)
+    {
+        meshRenderer.material = hover;
+    }
+}
