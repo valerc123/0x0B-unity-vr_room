b13136ba4113df708faaefca9c53b21d16d406af
valerc123 <valentina.rua123@gmail.com>
2021-09-12 22:55:16 -0500

    PARTICLE PASS CODE

--- Assets/Scripts/particles.cs
+++ Assets/Scripts/particles.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class particles : MonoBehaviour
 {
-    public GameObject particlesSystem;
+    public GameObject canvas;
     private bool switchParticle = true;
 
+
+    void Start (){
+        canvas.SetActive(false);
+
+    }
     public void ActivateParticle()
     {
-        particlesSystem.SetActive(switchParticle);
+        canvas.SetActive(switchParticle);
         switchParticle = !switchParticle;
         Debug.Log("entro");
     }
---
+++ Assets/Scripts/passcodeparticle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class passcodeparticle : MonoBehaviour
+{
+    public InputField inputField;
+    public GameObject particlesystem;
+    public GameObject canvas;
+
+   
+    public void checkInput(){
+        if (inputField.text == "1010"){
+            Debug.Log("accepted");
+            particlesystem.SetActive(true);
+            canvas.SetActive(false);
+        }
+    }
+}
